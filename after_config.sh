95271846ef066aed7038d8b6adf7aafc8497ee27
namysl <e.k.namysl@gmail.com>
2022-12-08 16:57:17 +0100

    changed the script

--- after_config.sh
+++ after_config.sh
@@ -10,11 +10,22 @@ helm install grafana grafana/grafana
 kubectl expose service grafana --type=NodePort --target-port=3000 --name=grafana-np
 kubectl patch svc grafana-np --type='json' --patch='[{"op": "replace", "path": "/spec/ports/0/nodePort", "value":30003}]'
 
+
+echo "Next steps needs to be performed manually:
+
+- ENABLE DASHBOARD:
 minikube dashboard &
 kubectl proxy --address='0.0.0.0' --disable-filter=true &
 
-kubectl port-forward service/nginx-on-minikube --address 0.0.0.0 30001:80 &
-kubectl port-forward service/prometheus-server-ng --address 0.0.0.0 30002:80 &
+- PORT FORWARD THE SERVICES:
+kubectl port-forward service/nginx-kube --address 0.0.0.0 30001:80 &
+kubectl port-forward service/prometheus-server-np --address 0.0.0.0 30002:80 &
 kubectl port-forward service/grafana-np --address 0.0.0.0 30003:80 &
 
+- GET ACCESS TO GRAFANA:
 kubectl get secret --namespace default grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
+minikube service --url nginx-kube
+
+- OPTIONALLY CHECK LOGS:
+kubectl logs deployment/<deployment-name>
+kubectl logs service/<service-name>"
