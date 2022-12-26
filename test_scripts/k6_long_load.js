50147526e1955d2bd41a883e662b7d5a8e6f744c
namysl <e.k.namysl@gmail.com>
2022-12-26 13:25:54 +0100

    fixed redundancy in stages

--- test_scripts/k6_long_load.js
+++ test_scripts/k6_long_load.js
@@ -4,11 +4,9 @@ import { sleep } from 'k6';
 export const options = {
   stages: [
     { duration: '3m', target: 50 },
-    { duration: '5m', target: 100 },
-    { duration: '5m', target: 200 },
+    { duration: '10m', target: 200 },
     { duration: '10m', target: 300 },
-    { duration: '5m', target: 200 },
-    { duration: '5m', target: 100 },
+    { duration: '10m', target: 100 },
     { duration: '5m', target: 50 },
     { duration: '2m', target: 10 },
     { duration: '10m', target: 0 },
